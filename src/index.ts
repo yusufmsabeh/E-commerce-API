7884579e001c715e3aa2714412a8ea3b11c55312
yusufmsabeh <yusuf.msabeh@gmail.com>
2023-07-13 00:48:31 +0300

    fix: resolve confilect

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import { router as testRouter } from "./routes/test";
-import {router as productsRouter} from "./routes/products";
+
+import { router as productsRouter } from "./routes/products";
+
+import { router as categoriesRouter } from "./routes/categories";
+
 import connection from "./database/config";
 dotenv.config();
 const app = express();
 const PORT = parseInt(process.env.PORT ?? "3000");
 app.use("/test", testRouter);
-app.use("/products",productsRouter);
+app.use("/products", productsRouter);
+app.use("/categories", categoriesRouter);
+
 connection.authenticate().then(
   () => {
     connection.sync().then(() => {
