89e0fa9e3892ff380c29630b61fb77c9f7ad21cc
yusufmsabeh <yusuf.msabeh@gmail.com>
2023-07-12 11:10:48 +0300

    fix: add try/catch

--- src/controllers/categories.ts
+++ src/controllers/categories.ts
@@ -1,13 +1,21 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { Category } from "../models/Category";
 
-export const getCategories = async (request: Request, response: Response) => {
-  const categories = await Category.findAll();
-  response.status(200).json({
-    error: false,
-    status: 200,
-    data: {
-      categories: categories,
-    },
-  });
+export const getCategories = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  try {
+    const categories = await Category.findAll();
+    return response.status(200).json({
+      error: false,
+      status: 200,
+      data: {
+        categories: categories,
+      },
+    });
+  } catch (e) {
+    next(e);
+  }
 };
