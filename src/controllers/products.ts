63728003388bf5c3032b7c39ca1575e28939469b
yusufmsabeh <yusuf.msabeh@gmail.com>
2023-07-13 00:00:29 +0300

    feat: retrieve handpicked collections

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { Product } from "../models/Product";
-import {Op} from "sequelize";
+import { Op } from "sequelize";
 
 export const getProducts = async (
   request: Request,
@@ -8,23 +8,32 @@ export const getProducts = async (
   next: NextFunction
 ) => {
   try {
-    const { q="" , page = 0 } = request.query;
+    const { q = "", page = 0 } = request.query;
     const startingOffset = parseInt(page as string) * 20 + 1;
     let whereObject = {};
-    if(q == "new-arrivals"){
+    if (q == "new-arrivals") {
       const threeMonthsAgo = new Date();
-      threeMonthsAgo.setMonth(threeMonthsAgo.getMonth()-3);
+      threeMonthsAgo.setMonth(threeMonthsAgo.getMonth() - 3);
       console.log(threeMonthsAgo);
-      whereObject={
-        createdAt:{
-          [Op.gte]:threeMonthsAgo
-        }
+      whereObject = {
+        createdAt: {
+          [Op.gte]: threeMonthsAgo,
+        },
+      };
+    } else if (q == "handpicked") {
+      whereObject = {
+        rate: {
+          [Op.gte]: 4.5,
+        },
+        price: {
+          [Op.lte]: 100,
+        },
       };
     }
     const products = await Product.findAll({
       offset: startingOffset,
       limit: 20,
-      where:whereObject
+      where: whereObject,
     });
     return response.status(200).json({
       error: false,
