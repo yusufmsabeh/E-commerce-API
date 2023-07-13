fc2b4ede424f3be2ed2d36da22c8f09e7bf1f1a4
yusufmsabeh <yusuf.msabeh@gmail.com>
2023-07-13 18:44:57 +0300

    feat: Retrieve category's products and refactor getProducts controller

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -2,8 +2,8 @@ import { NextFunction, Request, Response } from "express";
 import { Product } from "../models/Product";
 import { Op } from "sequelize";
 import { ProductImages } from "../models/Product-Images";
-import {Category} from "../models/Category";
-import {Brand} from "../models/Brand";
+import { Category } from "../models/Category";
+import { Brand } from "../models/Brand";
 
 export const getProducts = async (
   request: Request,
@@ -11,43 +11,78 @@ export const getProducts = async (
   next: NextFunction
 ) => {
   try {
-    const { q = "", page = 0 } = request.query;
+    const { q = "", page = 0, category } = request.query;
     const startingOffset = parseInt(page as string) * 20;
-    let whereObject = {};
-    if (q == "new-arrivals") {
-      const threeMonthsAgo = new Date();
-      threeMonthsAgo.setMonth(threeMonthsAgo.getMonth() - 3);
-      console.log(threeMonthsAgo);
-      whereObject = {
-        createdAt: {
-          [Op.gte]: threeMonthsAgo,
-        },
-      };
-    } else if (q == "handpicked") {
-      whereObject = {
-        rate: {
-          [Op.gte]: 4.5,
-        },
-        price: {
-          [Op.lte]: 100,
-        },
-      };
+    let products: Product[] = [];
+    if (category) {
+      products = await getCategoryProducts(
+        startingOffset,
+        parseInt(category as string)
+      );
+    } else {
+      if (q == "new-arrivals") {
+        products = await getNewArrivals(startingOffset);
+      } else if (q == "handpicked") {
+        products = await getHandPicked(startingOffset);
+      } else {
+        products = await Product.findAll({ offset: startingOffset, limit: 20 ,include:[ProductImages,Brand,Category]});
+      }
     }
-    const products = await Product.findAll({
-      include: [ProductImages,Brand,Category],
-      offset: startingOffset,
-      limit: 20,
-      where: whereObject,
-    });
     return response.status(200).json({
       error: false,
       status: 200,
       data: {
         products: products,
       },
-
     });
   } catch (e) {
     next(e);
   }
+};
+
+const getNewArrivals = async (startingOffset: number): Promise<Product[]> => {
+  const threeMonthsAgo = new Date();
+  threeMonthsAgo.setMonth(threeMonthsAgo.getMonth() - 3);
+  const products = await Product.findAll({
+    include: [ProductImages, Brand, Category],
+    offset: startingOffset,
+    limit: 20,
+    where: {
+      createdAt: {
+        [Op.gte]: threeMonthsAgo,
+      },
+    },
+  });
+  return products;
+};
+
+const getHandPicked = async (startingOffset: number): Promise<Product[]> => {
+  const products = await Product.findAll({
+    include: [ProductImages, Brand, Category],
+    offset: startingOffset,
+    limit: 20,
+    where: {
+      rate: {
+        [Op.gte]: 4.5,
+      },
+      price: {
+        [Op.lte]: 100,
+      },
+    },
+  });
+  return products;
+};
+
+const getCategoryProducts = async (
+  startingOffset: number,
+  categoryId: number
+): Promise<Product[]> => {
+  const category = await Category.findByPk(categoryId);
+  const products =
+    (await category?.$get("products", {
+      include: [ProductImages, Brand, Category],
+      offset: startingOffset,
+      limit: 20,
+    })) ?? [];
+  return products;
 };
