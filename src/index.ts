e350303e2c4f147ab5e31749eb14a65cab9eb709
yusufmsabeh <yusuf.msabeh@gmail.com>
2023-07-14 09:16:09 +0300

    fix3

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,14 @@ import { router as categoriesRouter } from "./routes/categories";
 import connection from "./database/config";
 dotenv.config();
 const app = express();
-const PORT = parseInt(process.env.PORT ?? "3000");
+const PORT = parseInt(process.env.PORT ?? "8080");
 app.use("/test", testRouter);
 app.use("/categories", categoriesRouter);
 
 connection.authenticate().then(
   () => {
     connection.sync().then(() => {
-      app.listen(PORT, "0.0.0.0", () => {
+      app.listen(PORT, () => {
         console.log("server is listening on port ", PORT);
       });
     });
