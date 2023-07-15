d95b9e2118d0c8f389ce05043ab11a0b117f7a3e
yusufmsabeh <yusuf.msabeh@gmail.com>
2023-07-15 21:10:25 +0300

    feat: handle sync and asyn errors using express-async-errors and remove unesessary try/catch

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import "express-async-errors";
 import { router as testRouter } from "./routes/test";
 import { router as categoriesRouter } from "./routes/categories";
 import { router as brandsRouter } from "./routes/brands";
 import { router as productsRouter } from "./routes/products";
 import connection from "./database/config";
+import {errorHandler} from "./middlewares/error-handler";
 dotenv.config();
 const app = express();
 const PORT = parseInt(process.env.PORT ?? "3000");
@@ -12,6 +14,7 @@ app.use("/test", testRouter);
 app.use("/categories", categoriesRouter);
 app.use("/products", productsRouter);
 app.use("/brands", brandsRouter);
+app.use(errorHandler);
 connection.authenticate().then(
   () => {
     connection.sync().then(() => {
---
+++ src/middlewares/error-handler.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express";
+
+export const errorHandler = (
+  error: Error,
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  console.log("error ", error.message);
+  response.status(500).json({
+    error: true,
+    status: 500,
+    message: "Internal server error",
+  });
+};
