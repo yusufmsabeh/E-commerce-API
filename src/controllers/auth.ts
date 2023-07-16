76eb8ca936ac41f408e4bd3a53df1a64bfa332d6
yusufmsabeh <yusuf.msabeh@gmail.com>
2023-07-16 13:33:19 +0300

    feat: protect private routes and add a test private route

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,7 +1,11 @@
-import { NextFunction, Request, RequestHandler } from "express";
-import { hash } from "bcrypt";
+import {NextFunction, Request, RequestHandler, Response, response} from "express";
+import { hash, compareSync } from "bcrypt";
 import { User } from "../models/User";
-export const signup: RequestHandler = async (request, response, next) => {
+import * as jwt from "jsonwebtoken";
+export const signup: RequestHandler = async (
+  request: Request,
+  response: Response,
+  next: NextFunction) => {
   const { firstName, lastName, mobile, email, dateOfBirth, password } =
     request.body;
   console.log(
@@ -29,6 +33,45 @@ export const signup: RequestHandler = async (request, response, next) => {
     status: 200,
     data: {
       message: "Signed up successfully",
+    },
+  });
+};
+export const login: RequestHandler = async (
+  request: Request,
+  response: Response,
+  next: NextFunction) => {
+  const { email, password } = request.body;
+  const user = await User.findOne({ where: { email: email } });
+  if (!user)
+    return response.status(404).json({
+      error: true,
+      status: 404,
+      data: { message: "There is no user with email" },
+    });
+
+  if (!compareSync(password, user.password))
+    return response.status(400).json({
+      error: true,
+      status: 400,
+      data: { message: "password is wrong" },
+    });
+  const payload = {
+    userId: user.id,
+  };
+  const secretKey = process.env.SECRET_KEY ?? "secret";
+  const token: string = jwt.sign(payload, secretKey, { expiresIn: "30d" });
+  response.status(200).json({
+    error: false,
+    status: 200,
+    data: {
+      user: {
+        firstName: user.first_name,
+        lastName: user.last_name,
+        email: user.email,
+        mobile: user.mobile,
+        dateOfBirth: user.date_of_birth,
+        token: token,
+      },
     },
   });
 };
