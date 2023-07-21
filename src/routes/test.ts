32f60a1d349d2b11a0c5dd013c1f1f55c8e3db65
yusufmsabeh <yusuf.msabeh@gmail.com>
2023-07-21 19:59:21 +0300

    add txt for testing

--- src/routes/test.ts
+++ src/routes/test.ts
@@ -5,5 +5,8 @@ import passport from "passport";
 export const router = Router();
 
 router.get("/", getTest);
+router.get("/reqbin-verify.txt",(req,res,next)=>{
+  res.status(200).send("");
+});
 router.use(passport.authenticate("jwt", { session: false }));
 router.get("/private", getPrivateRoute);
