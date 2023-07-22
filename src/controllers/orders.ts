5fc366120908827ffd4756b360210a98609de9b2
yusufmsabeh <yusuf.msabeh@gmail.com>
2023-07-22 23:43:17 +0300

    feat: improve input validation

--- src/controllers/orders.ts
+++ src/controllers/orders.ts
@@ -3,7 +3,9 @@ import { User } from "../models/User";
 import { Order } from "../models/Order";
 import { Cart } from "../models/Cart";
 import { GeneralError } from "../errors/general-error";
-import {postOrdersValidator} from "../validators/post-orders-validator";
+import { postOrdersValidator } from "../validators/post-orders-validator";
+import { ValidationError } from "../errors/validation";
+import { ValidationErrorItem } from "sequelize";
 
 export const postOrders: RequestHandler = async (
   request: Request,
@@ -20,20 +22,25 @@ export const postOrders: RequestHandler = async (
     cart.status = 1;
     await cart.save();
     if (user) {
-
-      order= await user.$create("order", {
+      order = await user.$create("order", {
         email: user.email,
         status: 0,
         cart_id: cartId,
         transaction_id: transactionId,
       });
-    } else {
+    } else if (email) {
       order = await Order.create({
         email: email,
         status: 0,
         user_id: null,
         cart_id: cartId,
         transaction_id: transactionId,
+      });
+    } else {
+      return response.status(422).json({
+        error: true,
+        status: 422,
+        message: "email address can not be null",
       });
     }
     response.status(201).json({
