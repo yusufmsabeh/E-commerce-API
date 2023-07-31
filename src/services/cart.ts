2c57632aade9ccbae5a791603bfcef4111a3741e
yusufmsabeh <yusuf.msabeh@gmail.com>
2023-07-31 14:28:07 +0300

    fix: updateTotal method so it can calculate the total if the discount-type is fixed of percentage

--- src/services/cart.ts
+++ src/services/cart.ts
@@ -1,28 +1,36 @@
-import {Cart} from "../models/Cart";
-import {Product} from "../models/Product";
-import {getCartProducts} from "./cart-product";
+import { Cart } from "../models/Cart";
+import { Product } from "../models/Product";
+import { getCartProducts } from "./cart-product";
 import getConnection from "../database/config";
-import {CART_STATUS} from "../enums/status-enums";
+import { CART_STATUS } from "../enums/status-enums";
+import { Sequelize } from "sequelize-typescript";
 
-export const getCart = async (user_id:string)=>{
+export const getCart = async (user_id: string) => {
   const [cart] = await Cart.findOrCreate<Cart>({
     where: { status: CART_STATUS.IN_PROGRESS, user_id: user_id },
   });
   return cart;
 };
 
-export const getActiveCartById = async (cartId:string)=>{
-  return  await Cart.findOne({
+export const getActiveCartById = async (cartId: string) => {
+  return await Cart.findOne({
     where: { id: cartId, status: CART_STATUS.IN_PROGRESS },
   });
 };
 
-export const updateTotalPrice = async (cart:Cart)=>{
-
+export const updateTotalPrice = async (cart: Cart) => {
   const connection = getConnection();
   const result: any = await getCartProducts({
     attributes: [
-      [connection.literal("quantity*products.price"), "totalPrice"],
+      [
+        connection.fn(
+          "sum",
+          connection.literal(
+            "CASE WHEN products.discount_type = 'percentage' THEN (quantity * products.price) * (100 - products.discount) / 100 WHEN products.discount_type = 'fixed' THEN (quantity * products.price) - products.discount * quantity END"
+          )
+        ),
+        "totalPrice",
+      ],
     ],
     include: [
       {
@@ -37,8 +45,8 @@ export const updateTotalPrice = async (cart:Cart)=>{
     raw: true,
   });
 
-  cart.total_cost=0;
-  if (result.length>0) {
+  cart.total_cost = 0;
+  if (result.length > 0) {
     const totalPrice = result[0].totalPrice ?? 0;
     const cartDiscount: number = cart.discount / 100;
     const tax = cart.tax;
